a63ec40bae3baf45a5f771472d97892721e75310
Mariusz <mariusz.pankiewicz@nist.de>
2020-02-01 22:16:14 +0100

    marker

--- src/Components/Home/Home.component.tsx
+++ src/Components/Home/Home.component.tsx
@@ -1,8 +1,4 @@
 import React, { Fragment, FunctionComponent } from 'react'
-import Firebase from '../../Firebase/firebase';
-import * as ROUTES from '../../Constants/routes'
-import { useForm, OnSubmit } from 'react-hook-form'
-import { Link, navigate } from '@reach/router';
 import { useUser } from '../../Contexts/user.context'
 import { LeafletMap } from '../Map/Map.component';
 
