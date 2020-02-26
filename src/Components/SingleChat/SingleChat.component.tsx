8bd5df6ac0d3c3ab7def2c32d585798cc443140e
aethel <aethelbrihte@gmail.com>
2020-02-26 16:32:19 +0100

    single chat page

--- src/Components/SingleChat/SingleChat.component.tsx
+++ src/Components/SingleChat/SingleChat.component.tsx
@@ -20,58 +20,40 @@ const SingleChatComponent: FunctionComponent<Props> = ({ ...props }) => {
   const { targetChatID } = props.routeProps;
   const [state, setState] = useState<any>();
   const { user } = useUser();
-  const existingChatID: string = state?.id || 'abc';
-  const targetUserID = () =>
-    state?.data().members.find((id: string) => id !== user.uid);
+
+  const targetUserID = (array: string[]) =>
+    array.find((id: string) => id !== user.uid);
 
   useEffect(() => {
     const unsubscribe: any = firebase!
       .getChats()
       .doc(targetChatID)
       .onSnapshot((res: any) => {
-          setState(res);
+        setState(res);
+        console.log(state);
       });
     return () => unsubscribe();
   }, []);
 
   return (
     <div>
-      {!state?.length && <p>no chat</p>}
-      {state?.data().messages.map(
-        (item: any, index: number) => {
-          return (
-            // <details className="chatBox" key={`${index}${targetChatID}`}>
-            //   <summary>
-            //     {item[0].fromName},{" "}
-            //     {new Date(item[0].timestamp).toLocaleDateString("en-GB", {
-            //       weekday: "long",
-            //       year: "numeric",
-            //       month: "long",
-            //       day: "numeric",
-            //       hour: "numeric",
-            //       minute: "numeric"
-            //     })}
-            //   </summary>
-            //        <div>
-            //         <span> From: {item.fromName}</span> <p>{item.message}</p>
-            //       </div>
-         
-            //   <ChatInputComponent
-            //     firebase={firebase}
-            //     routeProps={{ targetUserID: targetUserID(), existingChatID }}
-            //   />
-            // </details>
-            <div key={`${index}${targetChatID}`}>
-                < span> From: {item.fromName}</span> <p>{item.message}</p>
-         
-            </div>
-          );
-        }
-        )}
+      {!state && <p>no chat</p>}
+      {state?.data().messages.map((item: any, index: number) => {
+        return (
+          <div key={`${index}${targetChatID}`}>
+            <span> From: {item.fromName}</span> <p>{item.message}</p>
+          </div>
+        );
+      })}
+      {state && (
         <ChatInputComponent
           firebase={firebase}
-          routeProps={{ targetUserID: targetUserID(), existingChatID }}
+          routeProps={{
+            targetUserID: targetUserID(state?.data().members),
+            existingChatID: state.id
+          }}
         />
+      )}
     </div>
   );
 };
