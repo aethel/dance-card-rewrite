342df9ded7119b895d78edf1d64cc02bba65149d
aethel <aethelbrihte@gmail.com>
2020-07-28 21:17:49 +0200

    Disable chats on profile inactive

--- src/Components/Header/Navigation.component.tsx
+++ src/Components/Header/Navigation.component.tsx
@@ -1,16 +1,29 @@
-import React, { FunctionComponent, Fragment } from "react";
-import Firebase from "../../Firebase/firebase";
-import { navigate, Link } from "@reach/router";
-import * as ROUTES from "../../Constants/routes";
-import { useUser } from "../../Contexts/user.context";
-import "./Navigation.component.css";
+import React, { FunctionComponent, useRef } from 'react';
+import Firebase from '../../Firebase/firebase';
+import { navigate, Link } from '@reach/router';
+import * as ROUTES from '../../Constants/routes';
+import { useUser } from '../../Contexts/user.context';
+import './Navigation.component.css';
+import { useProfile } from '../../Contexts/profile.context';
 type Props = {
   firebase: Firebase;
 };
 
 const NavigationComponent: FunctionComponent<Props> = ({ firebase }: Props) => {
   const { clearUser } = useUser();
+  const { profile } = useProfile();
   const isLoggedIn = firebase.getCurrentUser();
+
+  // useEffect(() => {
+  //   console.log('profile :>> ', profile);
+  //   if (!profile.active) {
+  //     chatLinkRef.current?.setAttribute('disabled', 'true');
+  //   } else {
+  //     chatLinkRef.current?.removeAttribute('disabled');
+  //   }
+  //   console.log('object', chatLinkRef);
+  // }, [profile]);
+
   return (
     <div>
       <nav>
@@ -20,18 +33,22 @@ const NavigationComponent: FunctionComponent<Props> = ({ firebase }: Props) => {
         <Link className="button" to={ROUTES.PROFILE}>
           Profile
         </Link>
-        <Link className="button" to={ROUTES.CHATS}>
+        <Link
+          className={`button ${profile.active ? '' : 'disabled'}`}
+          to={profile.active ? ROUTES.CHATS : './'}
+        >
           Chats
         </Link>
-      <button  className="button button-outline"
-        onClick={() => {
-          firebase.doSignOut();
-          localStorage.clear();
-          navigate(ROUTES.LOG_IN);
-        }}
-      >
-        Logout
-      </button>
+        <button
+          className="button button-outline"
+          onClick={() => {
+            firebase.doSignOut();
+            localStorage.clear();
+            navigate(ROUTES.LOG_IN);
+          }}
+        >
+          Logout
+        </button>
       </nav>
     </div>
   );
